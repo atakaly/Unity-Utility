80ac42c607030b472adf1eee1d78f78dc7c99114
atakaly <79188326+atakaly@users.noreply.github.com>
2022-08-19 21:33:50 +0300

    Input System

---
+++ InputSystem.cs
@@ -0,0 +1,104 @@
+using System;
+using UnityEngine;
+
+public class InputSystem : MonoSingleton<InputSystem>
+{
+    public static Action<SwipeDirection> OnSwipe;
+    public static Action<Transform, Vector3> OnPressed;
+
+    [SerializeField]
+    private float swipeThreshold;
+
+    private Vector3 startInputPosition;
+
+    private SwipeDirection swipeDirection;
+
+    void Update()
+    {
+#if UNITY_EDITOR
+        if (Input.GetMouseButtonDown(0))
+        {
+            startInputPosition = Input.mousePosition;
+            Ray ray = Camera.main.ScreenPointToRay(startInputPosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                OnPressed?.Invoke(hit.transform, Camera.main.ScreenToWorldPoint(Input.mousePosition));
+            }
+        }
+
+
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                swipeDirection = SwipeDetector(startInputPosition, Input.mousePosition);
+                OnSwipe?.Invoke(swipeDirection);
+            }
+        }
+#endif
+
+        Touch touch = Input.GetTouch(0);
+        
+        if(touch.phase == TouchPhase.Began)
+        {
+            startInputPosition = touch.position;
+            Ray ray = Camera.main.ScreenPointToRay(startInputPosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                OnPressed?.Invoke(hit.transform, Camera.main.ScreenToWorldPoint(startInputPosition));
+            }
+        }
+
+        if(touch.phase == TouchPhase.Moved)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(touch.position);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                swipeDirection = SwipeDetector(startInputPosition, touch.position);
+                OnSwipe?.Invoke(swipeDirection);
+            }
+        }
+    }
+
+    private SwipeDirection SwipeDetector(Vector2 inputStartPosition, Vector2 currentInputPosition)
+    {
+        Vector2 inputPositionDifference = currentInputPosition - inputStartPosition;
+
+        if (inputPositionDifference.magnitude > swipeThreshold)
+        {
+            if (Mathf.Abs(inputPositionDifference.x) > Mathf.Abs(inputPositionDifference.y))
+            {
+                if (inputPositionDifference.x < 0)
+                    return SwipeDirection.Left;
+                else
+                    return SwipeDirection.Right;
+            }
+            else
+            {
+                if (inputPositionDifference.y < 0)
+                    return SwipeDirection.Down;
+                else
+                    return SwipeDirection.Up;
+            }
+        }
+
+        return SwipeDirection.None;
+    }
+
+}
+
+public enum SwipeDirection
+{
+    None,
+    Left,
+    Right,
+    Up,
+    Down
+}
